2aead5cecbf7b4c0b96e27d608dee047f3a21516
mnc345 <mnc345@stern.nyu.edu>
2021-06-22 22:39:04 -0400

    Cocktail ID added

--- app/cocktail_recipes.py
+++ app/cocktail_recipes.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+
+from requests.models import encode_multipart_formdata
+
 import requests
 import json
 
@@ -11,7 +14,7 @@ import random
 # COCKTAIL_API = os.getenv("COCKTAIL_API") -- NOT SURE WE NEED THIS
 
 def liquor_type():
-    liquor = input("Please select a liquor type: ")
+    liquor = input("Please select a liquor type: ").lower()
     valid_selections = ["whiskey", "whisky", "beer", "port", "vermouth", "everclear", "absinthe", "cider", "brandy", "aperol", "wine", "gin", "vodka", "rum", "tequila"]
     if liquor not in valid_selections:
         print("OOPS, invalid liquor type. Please try again.")
@@ -25,30 +28,38 @@ def liquor_type():
     liquor_data = json.loads(response.text)
 
     drinks = liquor_data["drinks"]
-    random_drink = random.choice(drinks)
-    print("Cocktail choice:",random_drink["strDrink"])
+    # random_drink = random.choice(drinks)
+    # print("Cocktail choice:",random_drink["strDrink"])
+
+    while True:
+        random_drink = random.choice(drinks)
+        print("*Cocktail choice:",random_drink["strDrink"])
+
+        user_choice = input("Do you want this type of cocktail? If so, type 'yes' If no, hit enter: ").lower()
+        if  user_choice == "yes":
+            break
+
+    drink_id = random_drink["idDrink"]
+
+    print("*You have selected drink id",drink_id)
+
+    request_url_id = f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drink_id}"
+    id_response = requests.get(request_url_id)
+    id_data = json.loads(id_response.text)
+
+    for i in (id_data["drinks"]):
+        print(i["strInstructions"], "\n")
+        print(i["strIngredient1"])
+        print(i["strIngredient2"])
+        print(i["strIngredient3"])
+        print(i["strIngredient4"])
+        print(i["strIngredient5"])
+        print(i["strIngredient6"])
+        print(i["strIngredient7"])
+        print(i["strIngredient8"])
+        print(i["strIngredient9"])
 
 liquor_type()
-
-# def cocktails():
-#     cocktail_type = input(random_drink["strDrink"])
-#     request_url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={cocktail_type}"
-#     data = requests.get(request_url)
-#     tt = json.loads(data.text)
-
-#     for i in (tt["drinks"]):
-#         print(i["strInstructions"], "\n")
-#         print(i["strIngredient1"])
-#         print(i["strIngredient2"])
-#         print(i["strIngredient3"])
-#         print(i["strIngredient4"])
-#         print(i["strIngredient5"])
-#         print(i["strIngredient6"])
-#         print(i["strIngredient7"])
-#         print(i["strIngredient8"])
-#         print(i["strIngredient9"])
-
-# cocktails()
 
 # # import webbrowser
 # # def cocktails():
